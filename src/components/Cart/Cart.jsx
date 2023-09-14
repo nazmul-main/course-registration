3ef0199fda3541d60d687e6fcf414d851b05841e
Nazmul Hossen <101635882+nazmulhossen1@users.noreply.github.com>
2023-09-14 23:58:55 +0600

    added click handele, useStae, and props  to find course name on cart  componets.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,21 @@
-const Cart = () => {
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+
+
+/* eslint-disable no-unused-vars */
+const Cart = ({ selectedActor }) => {
+    console.log(selectedActor);
     return (
-        <div>
-            <h3>This is card</h3>
+        <div className="p-3 bg-[#FFF] rounded-md'">
+            <h3 className="text-center">Credit Hour Remaining 7 hr</h3>
+            {
+                selectedActor.map(actor => (
+
+                        
+                        <li key={actor.id} className="mb-2">{actor.title}</li>
+                ))
+            }
+
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -16,9 +16,16 @@ const Home = () => {
     }, []);
 
     const handleSelectCourse = (actor) => {
-       setSelectedActor([...selectedActor, actor])
-    }
-    console.log(selectedActor);
+        const isExist = selectedActor.find(item => item.id == actor.id)
+        if (isExist){
+           return alert('Alredy selected')
+        }
+        else{
+            setSelectedActor([...selectedActor, actor])
+        }
+        
+     }
+ 
 
 
 
@@ -26,11 +33,11 @@ const Home = () => {
     return (
         <main className='max-w-screen-xl mx-auto  px-4 md:px-8 lg-px-12 py-1 md:py-4 '>
             <h3 className='text-4xl font-bold text-center mb-6'>Course Registration</h3>
-            <div className='flex md:gap-5  items-start bg-lime-50'>
+            <div className='flex md:gap-5 justify-evenly items-start'>
                 <div className='  card-container grid gap-4 lg:grid-cols-3 '>
                     {
                         allCourses.map(actor => (
-                            <div key={actor.id} className='w-72 card p-3 bg-red-50 rounded-md'>
+                            <div key={actor.id} className='w-72 card p-3 bg-[#FFF] rounded-md'>
                                 <img className='rounded-md' src={actor?.img} alt="" />
                                 <h3 className='text-[#1C1B1B] font-bold text-xl'>{actor?.title}</h3>
                                 <p>{actor?.details}</p>
@@ -38,13 +45,13 @@ const Home = () => {
                                     <p>$ Price: {actor?.price}</p>
                                     <p>Credit: {actor?.credit}</p>
                                 </div>
-                                <button  className='text-center text-white font-semibold py-2 rounded-md w-full bg-[#2F80ED]'>Selact</button>
+                                <button onClick={() => handleSelectCourse(actor)}  className='text-center text-white font-semibold py-2 rounded-md w-full bg-[#2F80ED]'>Selact</button>
                             </div>
                         ))
                     }
                 </div>
                 <div className='cart'>
-                    <Cart></Cart>
+                    <Cart selectedActor={selectedActor}></Cart>
                 </div>
             </div>
         </main>
